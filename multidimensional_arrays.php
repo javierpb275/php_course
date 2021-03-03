8836aeb78421364dbc00804bf06d5d5c6d1f6485
javierpb275 <javierpb275@gmail.com>
2021-03-03 11:36:30 +0100

    multidimensional arrays

---
+++ multidimensional_arrays.php
@@ -0,0 +1,13 @@
+<?php 
+
+$friends = array(
+    array('alex', 20), 
+    array('caesar', 30), 
+    array('pepe', 40)
+);
+
+echo $friends[0][0] . '<br/>';
+echo $friends[1][0] . '<br/>';
+echo $friends[2][0] . '<br/>';
+
+?>
