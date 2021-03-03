5117be3ab02acedfc70038a54e4171834dbd6095
javierpb275 <javierpb275@gmail.com>
2021-03-03 13:37:47 +0100

    switch

---
+++ switch.php
@@ -0,0 +1,17 @@
+<?php 
+
+$month = 'January';
+
+switch($month) {
+    case 'January':
+        echo "January";
+    break;
+    case 'February':
+        echo "February";
+    break;
+    default: 
+        echo "whatever";
+    break;
+}
+
+?>
