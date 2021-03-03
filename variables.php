0ffe137c1abbe743ddfdbc4a2724187c1a055069
javierpb275 <javierpb275@gmail.com>
2021-03-03 10:47:48 +0100

    types for variables, how to concat variables with text and the function gettype() that shows the type of that variable

---
+++ variables.php
@@ -0,0 +1,32 @@
+<?php
+
+#String: text
+$name = 'Charles';
+$name = "Charles";
+
+#Integer: integer numbers
+$number = 5;
+
+#Double: decimal numbers
+$number2 = 5.5;
+
+#Iboolean: true or false
+$true = true;
+$false = false;
+
+#Array: list
+
+#Object: 
+
+#Class
+
+#Null: variable with no value asigned;
+$null;
+
+echo "Hello $name";
+echo ' ';
+echo 'The integer number is ' . $number;
+echo ' ';
+echo gettype($name);
+
+?>
