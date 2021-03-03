153c30fbae713aad7bf2030d8bcfef27e06e6e54
javierpb275 <javierpb275@gmail.com>
2021-03-03 14:56:11 +0100

    var_dump() gives us information about a variable

---
+++ var_dump.php
@@ -0,0 +1,17 @@
+<?php 
+
+$text = 'Pepe';
+$number = 7;
+$number2 = '3';
+$array = array('Pepe', 'Paco', 'Nacho');
+$associated_array = array('name' => 'Pepe', 'age' => 25);
+$boolean = false;
+
+//var_dump gives us the value of the variable and other properties
+//like the number of characters if it is a String or digits if it 
+//is a number and the type of that variable
+echo "<pre>";
+var_dump($boolean);
+echo "</pre>";
+
+?>
