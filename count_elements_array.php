906ea4869b2363d6a7d4176401eb56e790bfbe83
javierpb275 <javierpb275@gmail.com>
2021-03-03 12:01:39 +0100

    displayed months of the year using foreach

--- count_elements_array.php
+++ count_elements_array.php
@@ -10,4 +10,27 @@ $months = array(
 $last_month = count($months) - 1;
 echo $months[$last_month];
 
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Months of the year</title>
+</head>
+<body>
+    <h1>Months of the year</h1>
+    <ul>
+     <?php
+        foreach($months as $month) {
+            echo '<li>' . $month . '</li>';
+        }
+    ?>   
+    </ul>
+    
+</body>
+</html>
