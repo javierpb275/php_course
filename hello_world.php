6b40767bc08ae27b20eddebae99c1294b700458a
javierpb275 <javierpb275@gmail.com>
2021-03-03 10:29:48 +0100

    echo , how to comment and mixed html with php

--- hello_world.php
+++ hello_world.php
@@ -1,3 +1,29 @@
 <?php 
 
+// echo is used to show information on the screen
+
+# another comment
+
+/*
+to comment 
+several lines
+*/
+
+echo "hello world";
+
+
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>whatever</title>
+    </head>
+    <body>
+    <h1>
+    <?php 
+        echo "hello world";
+    ?>
+    </h1>
+    </body>
+</html>
