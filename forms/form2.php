187d0608115b06a400f4545867436ae44123f1a4
javierpb275 <javierpb275@gmail.com>
2021-03-04 14:38:39 +0100

    form2

---
+++ forms/form2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="validate_data_sent.php" method="post" name="" id="" class="">
+        <input type="text" placeholder="Name:" name="name" id="">
+        <br>
+        <input type="email" placeholder="Email:" name="email" id="">
+        <br>
+        <input type="submit" name="submit-form" value="send">
+    </form>
+    
+</body>
+</html> 
