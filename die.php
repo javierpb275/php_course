e03f40ac2aa59ef86c5cc2fe94f6cc816172dc0c
javierpb275 <javierpb275@gmail.com>
2021-03-04 11:11:05 +0100

    die() function in order to stop the execution of the rest od the code

---
+++ die.php
@@ -0,0 +1,25 @@
+<?php
+
+//die(): It allows us to stop the execution of the rest 
+//of the code completely.
+
+$variable = 'whatever';
+
+var_dump($variable);
+
+die();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WHATEVER</h1>
+</body>
+</html>
