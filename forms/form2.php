ba4fece77ca6a386b9d95176420bd9d958e53057
javierpb275 <javierpb275@gmail.com>
2021-03-04 15:01:12 +0100

    validating a form

--- forms/form2.php
+++ forms/form2.php
@@ -1,3 +1,27 @@
+<?php 
+    if(isset($_POST['submit'])) {
+
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+
+        if(!empty($name)) {
+
+            $name = trim($name);//remove white spaces
+            $name = htmlspecialchars($name);//turn html speacial characters into text
+            $name = stripslashes($name);//removes symbols like '/'
+
+            //7:20. 46 Validando un formulario
+
+            echo "username: " . $name . '<br>';
+        }
+
+        
+        echo "email: " . $email . '<br>';
+
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,12 +32,12 @@
 </head>
 <body>
 
-    <form action="validate_data_sent.php" method="post" name="" id="" class="">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" name="" id="" class="">
         <input type="text" placeholder="Name:" name="name" id="">
         <br>
         <input type="email" placeholder="Email:" name="email" id="">
         <br>
-        <input type="submit" name="submit-form" value="send">
+        <input type="submit" name="submit" value="send">
     </form>
     
 </body>
