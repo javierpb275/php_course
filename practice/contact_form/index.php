89864b09c087a7c613ff81f2f668f5f1047ec2e0
javierpb275 <javierpb275@gmail.com>
2021-03-05 12:02:24 +0100

    ending validation and sending email

--- practice/contact_form/index.php
+++ practice/contact_form/index.php
@@ -8,6 +8,7 @@ if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $message = $_POST['message'];
 
+    //check name
     if (!empty($name)) {
         $name = trim($name);
         $name = filter_var($name, FILTER_SANITIZE_STRING);
@@ -15,6 +16,7 @@ if (isset($_POST['submit'])) {
         $error .= 'Please fill out the field "name" <br>';
     }
 
+    //check email
     if (!empty($email)) {
         $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
@@ -24,6 +26,26 @@ if (isset($_POST['submit'])) {
          
     } else {
         $error .= 'Please fill out the field "email" <br>';
+    }
+
+    //check message
+    if (!empty($message)) {
+        $message = htmlspecialchars($message);
+        $message = trim($message);
+        $message = stripslashes($message);
+         
+    } else {
+        $error .= 'Please fill out the field "message" <br>';
+    }
+
+    if(!$error) {
+        $send_to = 'yourname@yourcompany.com';
+        $issue = 'Email sent from mywebsite.com';
+        $message_ready = "From: $name \n";
+        $message_ready .= "Email: $email \n";
+        $message_ready .= "Message: " . $message;
+
+        mail($send_to, $issue, $message_ready);
     }
 
 
