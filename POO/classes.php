caacc62dffe5a674595ab3b5ab6d0a55f93e6160
javierpb275 <javierpb275@gmail.com>
2021-03-05 13:12:49 +0100

    first explanation of Object Oriented Programming. Created class Person with its properties and its own method. created objects and accessing their properties and calling their method

--- POO/classes.php
+++ POO/classes.php
@@ -1,14 +1,29 @@
 <?php 
 
-$name = "Charles";
-$age = 23;
-$country = 'UK';
+class Person {
 
-$name2 = "Charles";
-$age2 = 23;
-$country2 = 'UK';
+    public $name;
+    public $age;
+    public $country;
 
-echo $name;
+    public function showInfo() {
+        echo 'Hello World <br>';
+    }
 
+}
+
+$person1 = new Person;
+$person1->name = 'Charles';
+$person1->age= 23;
+$person1->country = 'UK';
+
+$person1->showInfo();
+
+$person2 = new Person;
+$person2->name = 'Pepe';
+$person2->age= 40;
+$person2->country = 'Spain';
+
+echo $person2->name . ' is ' . $person2->age . '. <br>';
 
 ?>
