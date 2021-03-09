d2e087888b8a591947f15149f5de22437f4f402a
javierpb275 <javierpb275@gmail.com>
2021-03-09 09:43:06 +0100

    executing mysql queries by using the query method

--- pdo.php
+++ pdo.php
@@ -1,11 +1,25 @@
 <?php 
 
+$id = $_GET['a_user_id'];
+
 try {
 
     //parameter1:db we are working with, the host we wanna connect to, db name;
     //parameter2: username; parameter3: password
     $connection = new PDO('mysql:host=localhost;dbname=test_console', 'root', '');
     echo "Connection successful.";
+    echo '<br>';
+
+    //Query Method
+    $results = $connection->query("SELECT * FROM t_users WHERE a_user_id = $id");
+    //$results = $connection->query('INSERT INTO t_users VALUES(NULL, "Peter", "peter@gmail.com")');
+
+    foreach($results as $row) {
+        //print_r($row);
+        echo $row['a_user_id'] . ' - ' . $row['a_name'];
+        echo '<br>';
+    }
+
 } catch(PDOException $e) {
     echo $e->getMessage();
 }
