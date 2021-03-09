66f26fe8298c8be50dc4d0e7835d566fd9692d32
javierpb275 <javierpb275@gmail.com>
2021-03-09 13:17:13 +0100

    created html and css structure

---
+++ practice/pagination/index.view.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagination</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+
+    <h1>ITEMS</h1>
+    <section class="articles">
+        <ul>
+            <li>1. - Lorem ipsum dolor sit amet. Obcaecati animi!</li>
+            <li>2. - Lorem ipsum dolor sit amet. Obcaecati animi!</li>
+            <li>3. - Lorem ipsum dolor sit amet. Obcaecati animi!</li>
+            <li>4. - Lorem ipsum dolor sit amet. Obcaecati animi!</li>
+        </ul>
+    </section>
+
+    <section class="pagination">
+        <ul>
+            <li class="disabled">&laquo;</li><!--left arrow-->
+            <li class="active"><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">&raquo;</a></li><!--right arrow-->
+        </ul>
+    </section>
+
+    </div>
+</body>
+</html>
