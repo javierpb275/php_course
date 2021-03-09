273ea8fc86bf5ec806d83f5e0e0b05b0a9589217
javierpb275 <javierpb275@gmail.com>
2021-03-09 12:48:48 +0100

    how to use prepared statements with mysqli

--- mysqli/prepared_statements_mysqli.php
+++ mysqli/prepared_statements_mysqli.php
@@ -19,11 +19,21 @@ else {
         // d = double
 
     //bind_param(): what are the values that we wanna add (?, ?, ?).
-    //$statement->bind_param('sss');
+    $statement->bind_param('iss', $id, $name, $email);
 
+    $id = NULL;
+
+    if(isset($_GET['a_name']) && isset($_GET['a_email'])) {
+        $name = $_GET['a_name'];
+        $email = $_GET['a_email'];
+    }
+
+    $statement->execute();
     
     if($connection->affected_rows >= 1) {
         echo 'rows inserted: ' . $connection->affected_rows;
+    } else {
+        echo 'No data added';
     }
     
 }
