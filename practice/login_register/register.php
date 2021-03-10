220ee6bb5d48fa1b735025b2fcbcfacfa6f0e8e1
javierpb275 <javierpb275@gmail.com>
2021-03-10 11:57:40 +0100

    validating and encrypting password and sending data to the db in the register form

--- practice/login_register/register.php
+++ practice/login_register/register.php
@@ -9,15 +9,15 @@ if (isset($_SESSION['user'])) {
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     //strtolower(turn string to lowercase); filter_var(clean values)
     //FILTER_SANITIZE_STRING: in case a user tries to inject some code <b>
-    $user = filter_var(strtolower($_POST['user']), FILTER_SANITIZE_STRING);
+    $name = filter_var(strtolower($_POST['user']), FILTER_SANITIZE_STRING);
     $password = $_POST['password'];
     $password2 = $_POST['password2'];
 
-    //echo "$user . $password . $password2";
+   
 
     $error = '';
 
-    if (empty($user) or empty($password) or empty($password2)) {
+    if (empty($name) or empty($password) or empty($password2)) {
         $error .= '<li>Please fill out the fields.</li>';
     } else {
 
@@ -34,7 +34,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         WHERE a_name = :a_name LIMIT 1');
 
         $statement->execute(array(
-            ':a_name' => $user
+            ':a_name' => $name
         ));
 
         $result = $statement->fetch();
@@ -45,7 +45,32 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         }
 
+        //encrypt the password
+        $password = hash('sha512', $password);
+        $password2 = hash('sha512', $password2);
+
+        if ($password != $password2) {
+            $error .= '<li>Both passwords are not equal.</li>';
+
+        }
+
     }
+
+    if ($error == '') {
+
+        $statement = $connection->prepare('INSERT INTO t_user 
+        (a_id, a_name, a_password) VALUES (NULL, :a_name, :a_password)');
+        
+        $statement->execute(array(
+            ':a_name' => $name,
+            ':a_password' => $password
+        ));
+
+        header('Location: login.php');
+
+    }
+
+
 }
 
 require 'views/register.view.php';
