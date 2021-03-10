512413157ad0c6a3a94d7b7375240b1a09f27762
javierpb275 <javierpb275@gmail.com>
2021-03-10 10:31:51 +0100

    getting data from the register form

--- practice/login_register/register.php
+++ practice/login_register/register.php
@@ -1,5 +1,21 @@
 <?php
 
+session_start();
+
+if (isset($_SESSION['user'])) {
+    header('Location: index.php');
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //strtolower(turn string to lowercase); filter_var(clean values)
+    //FILTER_SANITIZE_STRING: in case a user tries to inject some code <b>
+    $user = filter_var(strtolower($_POST['user']), FILTER_SANITIZE_STRING);
+    $password = $_POST['password'];
+    $password2 = $_POST['password2'];
+
+    echo "$user . $password . $password2";
+}
+
 require 'views/register.view.php';
 
 ?>
