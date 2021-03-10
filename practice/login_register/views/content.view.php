85504210a301bdab227dbabeacbe981ac5b182d0
javierpb275 <javierpb275@gmail.com>
2021-03-10 09:37:06 +0100

    creating a login and register form. created the html of the content and its styles

---
+++ practice/login_register/views/content.view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=RocknRoll+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Content</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="title">Content of the website</h1>
+        <a href="close.php">Close session</a>
+        <hr class="border">
+        <div class="content">
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Tempore repellat nobis corrupti aut in expedita cum dolorem? 
+                Hic eos ex ut repellat autem delectus quibusdam quaerat. 
+                Adipisci mollitia ipsum eos?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Tempore repellat nobis corrupti aut in expedita cum dolorem? 
+                Hic eos ex ut repellat autem delectus quibusdam quaerat. 
+                Adipisci mollitia ipsum eos?</p>
+            </article>
+        </div>
+    </div>
+</body>
+</html>
